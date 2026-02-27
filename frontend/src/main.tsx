6f83ed2a405fac371301018a0c919348cf826445
Erisnilton Freitas <erisniltongf@gmail.com>
2026-02-27 13:19:20 -0300

    chore(frontend): atualiza configurações e integrações principais

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "virtual:uno.css";
+import "@unocss/reset/tailwind.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
+import { Toaster } from "sonner";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <CartProvider>
+        <App />
+        <Toaster position="top-right" richColors />
+      </CartProvider>
+    </AuthProvider>
   </StrictMode>,
-)
+);
